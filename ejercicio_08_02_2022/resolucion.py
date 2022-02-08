8dcdaaef1afaad84c1c7e8adde24b96137ef6e54
Cecilia Alonzo <92656251+cecialo@users.noreply.github.com>
2022-02-07 18:46:59 -0600

    mensaje

---
+++ ejercicio_08_02_2022/resolucion.py
@@ -0,0 +1,16 @@
+# Presentación del menú con los tipos de pizza
+print("Bienvenido a consiente tu vehículo.\nTipos de servicio\n\t1- lavado rápido\n\t2- lavado profundo\n\t3- Alquiler tipo A\n\t4- Alquiler tipo B\n\t5- Alquiler tipo C\n\t6- Estacionamiento por tres horas\n")
+tipo = input("Introduce el número correspondiente al tipo de servicio que quieres:")
+# Decisión sobre el tipo de servicio que quieres
+if tipo == "1":
+        print("$.5.00")
+if tipo == "2":
+        print("$.10.00")
+if tipo == "3":
+        print("$.100.00")
+if tipo == "4":
+        print("$.150.00")
+if tipo == "5":
+        print("$.200.00")
+if tipo == "6":
+        print("$.2.00")
